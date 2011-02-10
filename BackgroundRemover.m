76586fc82fbdfe15e6ed54df6077bf92b78db8f5
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-02-10 09:01:50 +0100

    Added BackgroundRemover for identification of objects of interest

---
+++ BackgroundRemover.m
@@ -0,0 +1,17 @@
+classdef BackgroundRemover
+    properties
+        maximumDistance;
+    end
+    methods
+        function obj = BackgroundRemover(maximumDistance)
+            obj.maximumDistance = maximumDistance;
+        end
+        function video = filter(self, video)
+            background = median(video, 4);
+            for i=1:size(video, 4)
+                mask = abs(video(:, :, :, i) - background) > self.maximumDistance;
+                video(:, :, :, i) = video(:, :, :, i) .* mask;
+            end
+        end
+    end
+end
---
+++ testBackgroundRemover.m
@@ -0,0 +1,35 @@
+function test_suite = testBackgroundRemover
+initTestSuite
+
+function testSetsToBlackAllBackgroundPixels
+video = ones(20, 10, 1, 3);
+video(1, 1, 1, 2) = 2;
+video(2, 2, 1, 3) = 2;
+remover = BackgroundRemover(0);
+result = remover.filter(video);
+% empty frame
+assertEqual(zeros(20, 10, 1), result(:, :, :, 1));
+% corner in second frame
+assertEqual(0, result(1, 2, 1, 2));
+assertEqual(2, result(1, 1, 1, 2));
+% pixel next to the corner in third frame
+assertEqual(0, result(1, 2, 1, 3));
+assertEqual(2, result(2, 2, 1, 3));
+
+function testBackgroundPixelsAreWithinADistanceFromBackgroundValue
+video = ones(20, 10, 1, 3) * 40;
+video(1, 1, 1, 2) = 42;
+video(1, 2, 1, 2) = 39;
+video(2, 2, 1, 3) = 42;
+video(1, 2, 1, 3) = 41;
+remover = BackgroundRemover(1);
+result = remover.filter(video);
+% empty frame
+assertEqual(zeros(20, 10, 1), result(:, :, :, 1));
+% corner in second frame
+assertEqual(0, result(1, 2, 1, 2));
+assertEqual(42, result(1, 1, 1, 2));
+% pixel next to the corner in third frame
+assertEqual(0, result(1, 2, 1, 3));
+assertEqual(42, result(2, 2, 1, 3));
+
