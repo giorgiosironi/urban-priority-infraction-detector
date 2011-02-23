2d7d1a48f3c26c00b9f3c3f4b0bbd5d43048783c
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-02-23 14:44:32 +0100

    Added pixel-wise construction by single pixel histograms

--- IntegralHistogram.m
+++ IntegralHistogram.m
@@ -6,8 +6,21 @@ classdef IntegralHistogram < handle
         function obj = IntegralHistogram(sizeX, sizeY)
             obj.content = cell(sizeX, sizeY);
         end
-        function setSingleHistogram(self, x, y, H)
+        function setCumulativeHistogramAt(self, x, y, H)
             self.content{x, y} = H;
+        end
+        function setPixel(self, x, y, singlePixelH)
+            total = singlePixelH;
+            if (x-1 > 0)
+                total = total + self.content{x-1, y};
+            end
+            if (y-1 > 0)
+                total = total + self.content{x, y-1};
+            end
+            if (x-1 > 0 && y-1 > 0)
+                total = total - self.content{x-1, y-1};
+            end
+            self.content{x, y} = total;
         end
         function H = getHistogram(self, area)
             H = self.content{area.maxX, area.maxY} - self.content{area.minX - 1, area.maxY} - self.content{area.maxX, area.minY - 1} + self.content{area.minX - 1, area.minY - 1};
--- testIntegralHistogram.m
+++ testIntegralHistogram.m
@@ -2,16 +2,32 @@ function test_suite = testIntegralHistogram
 initTestSuite;
 
 function testCalculatesHistogramOfAnAreaBySubtractionOfIntegralHistograms
-integralH = IntegralHistogram(3, 3);
-integralH.setSingleHistogram(1, 1, 1);
-integralH.setSingleHistogram(2, 1, 5);
-integralH.setSingleHistogram(3, 1, 10);
-integralH.setSingleHistogram(1, 2, 3);
-integralH.setSingleHistogram(2, 2, 13);
-integralH.setSingleHistogram(3, 2, 24);
-integralH.setSingleHistogram(1, 3, 6);
-integralH.setSingleHistogram(2, 3, 22);
-integralH.setSingleHistogram(3, 3, 40);
+integralH = getSampleCumulativeHistogram();
 H = integralH.getHistogram(Area.fromXYtoXY(2, 2, 3, 3));
 assertEqual(25, H);
+
+function testCalculatesItselfByAddingOnePixelAtTheTimeIfBuiltMonotonically
+integralH = IntegralHistogram(3, 3);
+integralH.setPixel(1, 1, 1);
+integralH.setPixel(2, 1, 4);
+integralH.setPixel(3, 1, 5);
+integralH.setPixel(1, 2, 2);
+integralH.setPixel(2, 2, 6);
+integralH.setPixel(3, 2, 6);
+integralH.setPixel(1, 3, 3);
+integralH.setPixel(2, 3, 6);
+integralH.setPixel(3, 3, 7);
+assertEqual(getSampleCumulativeHistogram(), integralH);
+
+function integralH = getSampleCumulativeHistogram
+integralH = IntegralHistogram(3, 3);
+integralH.setCumulativeHistogramAt(1, 1, 1);
+integralH.setCumulativeHistogramAt(2, 1, 5);
+integralH.setCumulativeHistogramAt(3, 1, 10);
+integralH.setCumulativeHistogramAt(1, 2, 3);
+integralH.setCumulativeHistogramAt(2, 2, 13);
+integralH.setCumulativeHistogramAt(3, 2, 24);
+integralH.setCumulativeHistogramAt(1, 3, 6);
+integralH.setCumulativeHistogramAt(2, 3, 22);
+integralH.setCumulativeHistogramAt(3, 3, 40);
 
