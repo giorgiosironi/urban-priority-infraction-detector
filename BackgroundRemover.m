0c125ea482588838037d887ce45d2f4351546096
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-02-25 11:24:25 +0100

    Added configuration of background pixels to custom value

--- BackgroundRemover.m
+++ BackgroundRemover.m
@@ -1,16 +1,29 @@
 classdef BackgroundRemover < handle
     properties
         maximumDistance;
+        backgroundValue;
     end
     methods
-        function obj = BackgroundRemover(maximumDistance)
+        function obj = BackgroundRemover(maximumDistance, backgroundValue)
             obj.maximumDistance = maximumDistance;
+            if (nargin == 2)
+                obj.backgroundValue = backgroundValue;
+            else
+                obj.backgroundValue = 0;
+            end
         end
         function video = filter(self, video)
             background = median(video, 4);
-            for i=1:size(video, 4)
-                mask = abs(video(:, :, :, i) - background) > self.maximumDistance;
-                video(:, :, :, i) = video(:, :, :, i) .* mask;
+            videoLength = size(video, 4);
+            channels = size(video, 3);
+            for i=1:videoLength
+                mask = (abs(video(:, :, :, i) - background) <= self.maximumDistance);
+                for j=1:channels
+                    channelsMask(:, :, j) = mask;
+                end
+                bg = channelsMask .* self.backgroundValue;
+                fg = ~channelsMask .* video(:, :, :, i);
+                video(:, :, :, i) = bg + fg;
             end
         end
     end
--- testBackgroundRemover.m
+++ testBackgroundRemover.m
@@ -33,3 +33,15 @@ assertEqual(42, result(1, 1, 1, 2));
 assertEqual(0, result(1, 2, 1, 3));
 assertEqual(42, result(2, 2, 1, 3));
 
+function testBackgroundPixelsCanBeSetToCustomValues
+video = ones(20, 10, 1, 3);
+video(1, 1, 1, 2) = 2;
+video(2, 2, 1, 3) = 2;
+remover = BackgroundRemover(0, -1);
+result = remover.filter(video);
+% empty frame
+assertEqual(ones(20, 10, 1) * -1, result(:, :, :, 1));
+assertEqual(-1, result(1, 2, 1, 2));
+assertEqual(-1, result(1, 2, 1, 3));
+
+
