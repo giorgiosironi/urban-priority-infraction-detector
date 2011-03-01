df7b7b9d31be4b65dbc9f0449531d8f013fee202
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-03-01 13:29:09 +0100

    Expanded algorithm for connected components detection: now works for every case

--- ObjectFinder.m
+++ ObjectFinder.m
@@ -25,6 +25,7 @@ classdef ObjectFinder < handle
             end
             labels = zeros(size(histograms));
             currentLabels = 0;
+            equivalences = [];
             for x=1:sizeOfGroup(1)
                 for y=1:sizeOfGroup(2)
                     if (isempty(histograms{x, y}))
@@ -32,14 +33,29 @@ classdef ObjectFinder < handle
                     end
                     westX = x;
                     westY = y-1;
+                    northX = x-1;
+                    northY = y;
+                    if (westY > 0 && northX > 0)
+                        if (~isempty(histograms{westX, westY}) && ~isempty(histograms{northX, northY}))
+                            firstLabel = labels(westX, westY);
+                            secondLabel = labels(northX, northY);
+                            if (firstLabel ~= secondLabel) 
+                                minimumLabel = min(firstLabel, secondLabel);
+                                maximumLabel = max(firstLabel, secondLabel);
+                                labels(x, y) = minimumLabel;
+                                equivalences = [equivalences; minimumLabel, maximumLabel];
+                            else 
+                                labels(x, y) = firstLabel;
+                            end
+                            continue;
+                        end
+                    end
                     if (westY > 0)
                         if (~isempty(histograms{westX, westY})) 
                             labels(x, y) = labels(westX, westY);
                             continue;
                         end
                     end
-                    northX = x-1;
-                    northY = y;
                     if (northX > 0)
                         if (~isempty(histograms{northX, northY}))
                             labels(x, y) = labels(northX, northY);
@@ -51,16 +67,26 @@ classdef ObjectFinder < handle
                     currentLabels = currentLabels + 1;
                 end
             end
+            for i=1:size(equivalences, 1)
+                from = equivalences(i, 1);
+                to = equivalences(i, 2);
+                indexes = find(labels == from);
+                labels(indexes) = to;
+            end
+            labels
             objects = cell(0);
             for label=1:currentLabels
                 areas = cell(0);
                 for x=1:sizeOfGroup(1)
                     for y=1:sizeOfGroup(2)
                         if (labels(x, y) == label)
+                            areas = [areas; {areaGroup.at(x, y)}];
                         end
                     end
                 end
-                objects = [objects; {TrackedObject(areas)}];
+                if (size(areas, 1) > 0)
+                    objects = [objects; {TrackedObject(areas)}];
+                end
             end
         end
     end
--- testObjectFinder.m
+++ testObjectFinder.m
@@ -7,4 +7,12 @@ content = ones(100) * -1;
 content(11:20, 11:40) = ones(10, 30);
 content(11:40, 51:60) = ones(30, 10);
 objects = finder.findIn(Frame(content));
-assertEqual([2 1], size(objects))
+assertEqual([2 1], size(objects));
+
+function testMergesObjectsWithNorthAndWestProtuberances
+finder = ObjectFinder(PatchesSelector(10, 10), ForegroundHistogramStrategy(), 1);
+content = ones(100) * -1;
+content(31:40, 11:40) = ones(10, 30);
+content(11:40, 31:40) = ones(30, 10);
+objects = finder.findIn(Frame(content));
+assertEqual([1 1], size(objects));
