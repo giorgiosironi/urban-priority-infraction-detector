5c323a1504963eef4ef071cf8bf1de9c48f8d70d
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-03-01 13:52:17 +0100

    Refactoring: extracted private methods

--- ObjectFinder.m
+++ ObjectFinder.m
@@ -12,17 +12,8 @@ classdef ObjectFinder < handle
         end
         function objects = findIn(self, frame)
             areaGroup = self.areaGroupSelector.getAreaGroup(frame.getArea());
-            sizeOfGroup = areaGroup.size();
-            histograms = cell(sizeOfGroup);
-            for x=1:sizeOfGroup(1)
-                for y=1:sizeOfGroup(2)
-                    patch = frame.cut(areaGroup.at(x, y));
-                    histogram = self.histogramStrategy.fromPixelData(patch);
-                    if (histogram.isValid(self.backgroundThreshold))
-                        histograms{x, y} = histogram;
-                    end
-                end
-            end
+            histograms = self.getForegroundAreas(frame, areaGroup);
+            sizeOfGroup = size(histograms);
             labels = zeros(size(histograms));
             currentLabels = 0;
             equivalences = [];
@@ -35,47 +26,64 @@ classdef ObjectFinder < handle
                     westY = y-1;
                     northX = x-1;
                     northY = y;
-                    if (westY > 0 && northX > 0)
-                        if (~isempty(histograms{westX, westY}) && ~isempty(histograms{northX, northY}))
-                            firstLabel = labels(westX, westY);
-                            secondLabel = labels(northX, northY);
-                            if (firstLabel ~= secondLabel) 
-                                minimumLabel = min(firstLabel, secondLabel);
-                                maximumLabel = max(firstLabel, secondLabel);
-                                labels(x, y) = minimumLabel;
-                                equivalences = [equivalences; minimumLabel, maximumLabel];
-                            else 
-                                labels(x, y) = firstLabel;
-                            end
-                            continue;
+                    if (self.isValid(histograms, westX, westY) && self.isValid(histograms, northX, northY))
+                        firstLabel = labels(westX, westY);
+                        secondLabel = labels(northX, northY);
+                        if (firstLabel ~= secondLabel) 
+                            minimumLabel = min(firstLabel, secondLabel);
+                            maximumLabel = max(firstLabel, secondLabel);
+                            labels(x, y) = minimumLabel;
+                            equivalences = [equivalences; minimumLabel, maximumLabel];
+                        else 
+                            labels(x, y) = firstLabel;
                         end
+                    elseif (self.isValid(histograms, westX, westY))
+                        labels(x, y) = labels(westX, westY);
+                    elseif (self.isValid(histograms, northX, northY))
+                        labels(x, y) = labels(northX, northY);
+                    else
+                        currentLabels = currentLabels + 1;
+                        labels(x, y) = currentLabels;
                     end
-                    if (westY > 0)
-                        if (~isempty(histograms{westX, westY})) 
-                            labels(x, y) = labels(westX, westY);
-                            continue;
-                        end
-                    end
-                    if (northX > 0)
-                        if (~isempty(histograms{northX, northY}))
-                            labels(x, y) = labels(northX, northY);
-                            continue;
-                        end
-                    end
-
-                    labels(x, y) = currentLabels + 1;
-                    currentLabels = currentLabels + 1;
                 end
             end
+            labels = self.unionOfEquivalences(labels, equivalences);
+            objects = self.extractObjectsBySameLabel(labels, areaGroup);
+        end
+    end
+    methods%(Access=private)
+        function histograms = getForegroundAreas(self, frame, areaGroup)
+            sizeOfGroup = areaGroup.size();
+            histograms = cell(sizeOfGroup);
+            for x=1:sizeOfGroup(1)
+                for y=1:sizeOfGroup(2)
+                    patch = frame.cut(areaGroup.at(x, y));
+                    histogram = self.histogramStrategy.fromPixelData(patch);
+                    if (histogram.isValid(self.backgroundThreshold))
+                        histograms{x, y} = histogram;
+                    end
+                end
+            end
+        end
+        function labels = unionOfEquivalences(self, labels, equivalences)
             for i=1:size(equivalences, 1)
                 from = equivalences(i, 1);
                 to = equivalences(i, 2);
                 indexes = find(labels == from);
                 labels(indexes) = to;
             end
-            labels
+        end
+        function b = isValid(self, histograms, x, y)
+            if (x <= 0|| y <= 0)
+                b = 0;
+                return;
+            end
+            b = ~isempty(histograms{x, y});
+        end
+        function objects = extractObjectsBySameLabel(self, labels, areaGroup)
+            sizeOfGroup = size(areaGroup);
             objects = cell(0);
-            for label=1:currentLabels
+            for label=1:max(max(labels))
                 areas = cell(0);
                 for x=1:sizeOfGroup(1)
                     for y=1:sizeOfGroup(2)
--- testObjectFinder.m
+++ testObjectFinder.m
@@ -5,9 +5,11 @@ function testProducesAListOfObjectsEachComposedByAreas
 finder = ObjectFinder(PatchesSelector(10, 10), ForegroundHistogramStrategy(), 1);
 content = ones(100) * -1;
 content(11:20, 11:40) = ones(10, 30);
-content(11:40, 51:60) = ones(30, 10);
+content(11:50, 51:60) = ones(40, 10);
 objects = finder.findIn(Frame(content));
 assertEqual([2 1], size(objects));
+assertEqual([3 1], size(objects{1}.areas));
+assertEqual([4 1], size(objects{2}.areas));
 
 function testMergesObjectsWithNorthAndWestProtuberances
 finder = ObjectFinder(PatchesSelector(10, 10), ForegroundHistogramStrategy(), 1);
@@ -16,3 +18,4 @@ content(31:40, 11:40) = ones(10, 30);
 content(11:40, 31:40) = ones(30, 10);
 objects = finder.findIn(Frame(content));
 assertEqual([1 1], size(objects));
+assertEqual([5 1], size(objects{1}.areas));
