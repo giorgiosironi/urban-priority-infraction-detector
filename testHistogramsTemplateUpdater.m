e99ac7e4c2217a945c428a24af6c62a784ee4401
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-03-03 13:23:30 +0100

    Refactoring: introduced setup()

--- testHistogramsTemplateUpdater.m
+++ testHistogramsTemplateUpdater.m
@@ -1,9 +1,10 @@
 function test_suite = testHistogramsTemplateUpdater
 initTestSuite;
 
-function testUpdatesHistogramsOfAMovedObjectByUsingTheNextFrame
+function histogramFactory = setup
 histogramFactory = IntegralHistogramFactory(GrayHistogramStrategy(2));
 
+function testUpdatesHistogramsOfAMovedObjectByUsingTheNextFrame(histogramFactory)
 movedObject = TrackedObjectPosition({Patch(GrayHistogram([0; 1]), Area.fromXYtoXY(10, 10, 12, 12))});
 nextFrame = zeros(20, 20); 
 nextFrame(10:12, 10:12) = ones(3, 3) * 255; %something interesting in the area
@@ -14,9 +15,8 @@ newObject = updater.updateTemplate(movedObject, newHistograms);
 assertEqual(Area.fromXYtoXY(10, 10, 12, 12), newObject.patches{1}.area);
 assertEqual(GrayHistogram([0; 9]), newObject.patches{1}.histogram);
 
-function testDoesNotUpdatePatchesWhichAreOccluded
+function testDoesNotUpdatePatchesWhichAreOccluded(histogramFactory)
 updater = HistogramsTemplateUpdater(MaximumDistanceAcceptanceStrategy(SimpleComparator(), 0.1));
-histogramFactory = IntegralHistogramFactory(GrayHistogramStrategy(2));
 movedObject = TrackedObjectPosition({Patch(GrayHistogram([1; 0]), Area.fromXYtoXY(10, 10, 12, 12))});
 nextFrame = zeros(20, 20); 
 nextFrame(10:12, 10:12) = ones(3, 3) * 255; %something very different in the area: likely to be an occlusion
