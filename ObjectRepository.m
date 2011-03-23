9177584a785ea819357bfded77f43dc8090ffd41
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-03-23 09:41:25 +0100

    Added tracking of already existing objects

--- ObjectRepository.m
+++ ObjectRepository.m
@@ -9,5 +9,10 @@ classdef ObjectRepository < handle
                 self.objects = [self.objects; {newObject}];
             end
         end
+        function trackObjects(self, oldPositions, newPositions, newFrame)
+            for i=1:size(oldPositions, 1)
+                self.objects{i}.addKnownPosition(newPositions{i}, newFrame);
+            end
+        end
     end
 end
--- testObjectRepository.m
+++ testObjectRepository.m
@@ -7,3 +7,12 @@ position2 = TrackedObjectPosition({Patch(NaN, NaN)});
 repository = ObjectRepository();
 repository.initializeObjects({position1; position2}, 40);
 assertEqual([2 1], size(repository.objects));
+
+function testAddsNewObjectsToTheStorage
+repository = ObjectRepository();
+oldPosition = TrackedObjectPosition({Patch(NaN, NaN)});
+repository.initializeObjects({oldPosition}, 40);
+newPosition = TrackedObjectPosition({Patch(NaN, NaN)});
+repository.trackObjects({oldPosition}, {newPosition}, 41);
+assertEqual([1 1], size(repository.objects));
+assertEqual([2 1], size(repository.objects{1}.frames));
