bb335d4f8c460cdfffe5dc19b4407b81bda972c2
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-06-02 13:40:17 +0200

    Added filters for projecting an object at its maximum speed throughout the scene

---
+++ MaxSpeedPositionFilter.m
@@ -0,0 +1,24 @@
+classdef MaxSpeedPositionFilter
+    methods
+        function newPositions = filterPositions(self, positions)
+            maxDisplacementNorm = 0;
+            for i=1:size(positions, 1)
+                 currentNorm = norm(double(positions{i}.displacementFromPrevious));
+                 if (currentNorm > maxDisplacementNorm)
+                     maxDisplacementNorm = currentNorm;
+                 end
+            end
+            newPositions = cell(0);
+            newPositions{1} = positions{1};
+            for i=2:size(positions, 1)
+                oldDisplacement = positions{i}.displacementFromPrevious;
+                correctionFactor = maxDisplacementNorm / norm(double(oldDisplacement));
+                newDisplacement = oldDisplacement * correctionFactor;
+                correctionDelta = newDisplacement - oldDisplacement;
+                newPosition = positions{i}.filter(LineMovementAreaFilter(correctionDelta));
+                newPosition = ObjectPosition(newPosition.areas, newDisplacement);
+                newPositions = [newPositions; {newPosition}];
+            end
+        end
+    end
+end
---
+++ testMaxSpeedPositionFilter.m
@@ -0,0 +1,22 @@
+function test_suite = testMaxSpeedPositionFilter
+initTestSuite;
+
+function testProjectsNextPositionsAtMaximumSpeed
+filter = MaxSpeedPositionFilter();
+positions = {getPosition([0 0], [0 0]); getPosition([100 0], [100 0]); getPosition([150 0], [50 0])};
+positions = filter.filterPositions(positions);
+expected = {getPosition([0 0], [0 0]); getPosition([100 0], [100 0]); getPosition([200 0], [100 0])};
+assertEqual(expected, positions);
+
+function testMaintainsOldDirectionsOfTheDisplacements
+filter = MaxSpeedPositionFilter();
+positions = {getPosition([0 0], [0 0]); getPosition([100 0], [100 0]); getPosition([100 100], [0 100])};
+expected = positions;
+positions = filter.filterPositions(positions);
+assertEqual(expected, positions);
+
+function p = getPosition(basePosition, displacement)
+p = ObjectPosition({Area.aroundPoint(basePosition(1), basePosition(2))}, displacement);
+
+
+
