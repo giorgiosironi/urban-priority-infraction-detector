f89c7f7938d4f5dc601be03ce2290964af9b8dbb
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-06-02 13:46:57 +0200

    Added additional case: cumulative displacement modification

--- MaxSpeedPositionFilter.m
+++ MaxSpeedPositionFilter.m
@@ -10,11 +10,13 @@ classdef MaxSpeedPositionFilter
             end
             newPositions = cell(0);
             newPositions{1} = positions{1};
+            cumulativeDisplacement = int16([0 0]);
             for i=2:size(positions, 1)
                 oldDisplacement = positions{i}.displacementFromPrevious;
                 correctionFactor = maxDisplacementNorm / norm(double(oldDisplacement));
                 newDisplacement = oldDisplacement * correctionFactor;
-                correctionDelta = newDisplacement - oldDisplacement;
+                correctionDelta = newDisplacement - oldDisplacement + cumulativeDisplacement;
+                cumulativeDisplacement = cumulativeDisplacement + correctionDelta;
                 newPosition = positions{i}.filter(LineMovementAreaFilter(correctionDelta));
                 newPosition = ObjectPosition(newPosition.areas, newDisplacement);
                 newPositions = [newPositions; {newPosition}];
--- testMaxSpeedPositionFilter.m
+++ testMaxSpeedPositionFilter.m
@@ -15,6 +15,13 @@ expected = positions;
 positions = filter.filterPositions(positions);
 assertEqual(expected, positions);
 
+function testCorrectionsInPositionsAndDisplacementAreCumulative
+filter = MaxSpeedPositionFilter();
+positions = {getPosition([0 0], [0 0]); getPosition([100 0], [100 0]); getPosition([150 0], [50 0]); getPosition([180 0], [30 0])};
+positions = filter.filterPositions(positions);
+expected = {getPosition([0 0], [0 0]); getPosition([100 0], [100 0]); getPosition([200 0], [100 0]); getPosition([300 0], [100 0])};
+assertEqual(expected, positions);
+
 function p = getPosition(basePosition, displacement)
 p = ObjectPosition({Area.aroundPoint(basePosition(1), basePosition(2))}, displacement);
 
